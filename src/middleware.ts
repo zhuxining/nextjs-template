1b4803115f6db7b2a760848bae1a823b2e9950a5
syneju <syneju@gmail.com>
2024-07-18 11:29:51 +0800

    update

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,14 @@
 import { auth } from "@/auth";
 
 export default auth((req) => {
-	const publicPaths = ["/", "/login", "/signup", "/api/auth"];
+	const publicPaths = ["/login", "/signup", "/api/auth"];
 
-	const isPublicRoute = publicPaths.some((path) =>
-		req.nextUrl.pathname.startsWith(path),
-	);
+	const isPublicRoute =
+		publicPaths.some((path) => req.nextUrl.pathname.startsWith(path)) ||
+		req.nextUrl.pathname === "/";
 
-	const isFile = /\.[\w]+$/.test(req.nextUrl.pathname);
-
-	if (!req.auth && !isPublicRoute && !isFile) {
-		const newUrl = new URL("/api/auth/signin", req.nextUrl.origin);
-		return Response.redirect(newUrl);
+	if (!req.auth && !isPublicRoute) {
+		return Response.redirect(new URL("/api/auth/signin", req.nextUrl.origin));
 	}
 });
 
